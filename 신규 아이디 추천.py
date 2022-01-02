c73f3118a637386c3842e1717e041f4617335691
onehousesilver <onehousesilver@gmail.com>
2022-01-03 00:33:08 +0900

    0102

--- 신규 아이디 추천.py
+++ 신규 아이디 추천.py
@@ -1,28 +1,45 @@
-alpa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-num = ['0','1','2','3','4','5','6','7','8','9']
+alpa = 'abcdefghijklmnopqrstuvwxyz'
+num = '0123456789'
+extra = '-_.'
 
 def solution(new_id):
-    # 1
-    first_change = new_id.lower()
-    # 2
-    second_change=[]
-    for i in first_change:
-        if i in alpa:
-            second_change.append(i)
-        elif i in num:
-            second_change.append(i)
-        elif i == '-':
-            second_change.append(i)
-        elif i =='_':
-            second_change.append(i)
-        elif i == '.':
-            second_change.append(i)
-    # 3
-    third_change=''
-    while '..' in second_change:
-        third_change = second_change.replace('..','.')
-        print(third_change)
-                
     answer = ''
+    # 1
+    answer = new_id.lower()
+    # 2
+    for i in answer:
+        if i in alpa or i in num or i in extra:
+            continue
+        else:
+            answer = answer.replace(i,'')
+    # 3
+    
+    while '..' in answer:
+        answer = answer.replace('..','.')
+    
+    # 4
+    if answer[0] == '.' and answer[-1] == '.':
+        answer = answer[1:-1]
+    elif answer[0] == '.':
+        answer = answer[1:]
+    elif answer[-1] == '.':
+        answer = answer[:-1]
+
+    # 5
+    if answer == "":
+        answer = answer.replace('','a')
+        
+    # 6
+    if len(answer) >= 16:
+        answer = answer[0:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+            
+    # 7
+    if len(answer) <= 2:
+        # while len(answer) == 3:
+        while len(answer) < 3:
+            answer = answer + answer[-1]
+
+            
     return answer
